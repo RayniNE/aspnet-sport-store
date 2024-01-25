e131cba0fbded6062aa4c68a982f40886226824b
Rayni Nuñez Espino <timetopwm@gmail.com>
2024-01-25 19:49:48 -0300

    Added new routes for handling different url patterns. Now we receive the page and the category.

--- SportsStore/Program.cs
+++ SportsStore/Program.cs
@@ -16,6 +16,13 @@ builder.Services.AddScoped<IStoreRepository, EFStoreRepository>();
 var app = builder.Build();
 
 app.UseStaticFiles();
+
+
+app.MapControllerRoute("catpage", "{category}/Page{productPage:int}", new { Controller = "Home", action = "Index" });
+app.MapControllerRoute("page", "Page{productPage:int}", new { Controller = "Home", action = "Index", productPage = 1 });
+app.MapControllerRoute("category", "{category}", new { Controller = "Home", action = "Index", productPage = 1 });
+app.MapControllerRoute("pagination", "Products/Page{productPage}", new { Controller = "Home", action = "Index", productPage = 1 });
+
 app.MapDefaultControllerRoute();
 
 SeedData.EnsurePopulated(app);
